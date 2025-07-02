eac0667a11c73d855c5a5349332b6d2b3c43ba33
YONKOYNK <141737146+YONKOYNK@users.noreply.github.com>
2025-07-03 01:50:22 +0200

    Update server.lua
    fix du problème d'argent

--- ynk_quickTrip/server.lua
+++ ynk_quickTrip/server.lua
@@ -1,17 +1,19 @@
 RegisterNetEvent('moneyRemove')
-AddEventHandler('moneyRemove', function()
+AddEventHandler('moneyRemove', function(price)
     local xPlayer = ESX.GetPlayerFromId(source)
 
-    if xPlayer then
-        local bankMoney = xPlayer.getAccount('bank').money
-        local cashMoney = xPlayer.getMoney()
+    local bankMoney = xPlayer.getAccount('bank').money
+    local cashMoney = xPlayer.getMoney()
 
-        if bankMoney >= 500 then
-            xPlayer.removeAccountMoney('bank', 500)
-        elseif bankMoney < 500 and cashMoney >= 500 then
-            xPlayer.removeMoney(500)
-        end
+    if (bankMoney + cashMoney) < price then
+        print('vous navez pas assez dargent')
+        return
+    end
+
+    -- Retirer d'abord sur le cash si suffisant
+    if cashMoney >= price then
+        xPlayer.removeMoney(price)
+    else
+        xPlayer.removeAccountMoney('bank', reste)
     end
 end)
-
-
